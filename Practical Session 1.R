2901f78e9ea28beddfcf255f9c95479c9a419474
harshita <L00171050@atu.ie.com>
2023-03-13 18:51:56 +0000

    Initial Commit

---
+++ Practical Session 1.R
@@ -0,0 +1,19 @@
+x <- c(3, 12, 7)
+y <- c(4, 9, 11)
+z <- x + y
+z
+x + 2
+y + 50
+x
+y
+sum(x)
+mean(y)
+w <- c(2, 7, NA, 20)
+sum(w)
+v <- sum(w, na.rm = TRUE)
+typeof(v) 
+x > y
+append(x, 12)
+u <- 5:20
+letters[6:10]
+append(u, letters[6:10])
